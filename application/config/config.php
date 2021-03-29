fe4c2f5dea60e1e4cadf6412ce4ca24216dcd4e2
mirzaromi <mirzaromi12@gmail.com>
2021-03-29 20:30:02 +0700

    Merubah database dan config base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sambangan.mai-au.sch.id/';
+$config['base_url'] = 'http://localhost/sambangan/';
 
 /*
 |--------------------------------------------------------------------------
