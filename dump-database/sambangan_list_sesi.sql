574bcf067b67320ba6b11c9dff9e21582d3fc7b0
Solork <sherry.solork@gmail.com>
2021-04-02 19:38:20 +0700

    database

---
+++ dump-database/sambangan_list_sesi.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `sambangan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sambangan`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sambangan
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `list_sesi`
+--
+
+DROP TABLE IF EXISTS `list_sesi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `list_sesi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `kuota` int NOT NULL,
+  `sisa` int NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam_mulai` varchar(6) NOT NULL,
+  `jam_selesai` varchar(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `list_sesi`
+--
+
+LOCK TABLES `list_sesi` WRITE;
+/*!40000 ALTER TABLE `list_sesi` DISABLE KEYS */;
+INSERT INTO `list_sesi` VALUES (9,20,18,'2021-04-02','10:00','12:00');
+/*!40000 ALTER TABLE `list_sesi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-02 19:37:20
---
+++ dump-database/sambangan_sesi_sambangan.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `sambangan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sambangan`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sambangan
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sesi_sambangan`
+--
+
+DROP TABLE IF EXISTS `sesi_sambangan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sesi_sambangan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` int DEFAULT NULL,
+  `nama_santri` varchar(200) NOT NULL,
+  `nama_walisantri` varchar(200) NOT NULL,
+  `kelas_santri` varchar(200) NOT NULL,
+  `is_aktif` int NOT NULL,
+  `is_offline` int NOT NULL,
+  `status` int NOT NULL,
+  `sesi` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_created` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=59 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sesi_sambangan`
+--
+
+LOCK TABLES `sesi_sambangan` WRITE;
+/*!40000 ALTER TABLE `sesi_sambangan` DISABLE KEYS */;
+INSERT INTO `sesi_sambangan` VALUES (1,47398,'AHMAD GHULAM RAMADHAN NUGROHO','BASUKI SETYO NUGROHO','XA',0,0,0,'sesi_4','2021-03-17 02:05:57','2021-03-17'),(4,89450,'ACHMAD YUSRON KAFABI','MUSLIM','XA',0,0,0,'sesi_4','2021-03-21 01:37:52','2021-03-16'),(58,47705,'ACHMAD RIFKI ADENAN','ACHMAD TOFANI ADENAN','XA',0,0,0,'9','2021-04-01 10:53:03','2021-04-01'),(57,47705,'ACHMAD RIFKI ADENAN','ACHMAD TOFANI ADENAN','XA',0,0,0,'9','2021-04-01 10:52:58','2021-04-01');
+/*!40000 ALTER TABLE `sesi_sambangan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-02 19:37:20
